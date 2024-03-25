a9f964bf55f0e6582a07a1338c6d5f705a8bb5ce
LNahuelFB <lnahuelfernandezb@gmail.com>
2024-03-25 06:03:47 -0300

    Terminada la maquetacion del formulario de la pagina 3 y agregados el navbar y el footer mediante javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('hola mundo')
+const navbar = document.getElementById("navbar")
+const footer = document.getElementById("footer")
+
+let actualUrl = window.location.pathname
+const year = new Date().getFullYear()
+
+console.log('Hola mundo!');
+
+navbar.innerHTML = `
+    <a href=${actualUrl = "/index.html" ? "/index.html" : "../index.html"} class="link">Inicio</a>
+    <a href="/pages/pagina1.html" class="link">Pagina 1</a>
+    <a href="/pages/pagina2.html" class="link">Pagina 2</a>
+    <a href="/pages/pagina3.html" class="link">Pagina 3</a>
+`;
+
+footer.innerHTML = `
+<p>Proyecto Full Stack con Python de Codo a Codo - ${year} </p>
+`

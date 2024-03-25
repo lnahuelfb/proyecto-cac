eb21bf30187c3685f0d5d71f9c4da0ad9776505d
LNahuelFB <lnahuelfernandezb@gmail.com>
2024-03-25 06:26:04 -0300

    Cambio de index.html a home.html en todos las etiquetas anchor

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const year = new Date().getFullYear()
 console.log('Hola mundo!');
 
 navbar.innerHTML = `
-    <a href=${actualUrl = "/index.html" ? "/index.html" : "../index.html"} class="link">Inicio</a>
+    <a href=${actualUrl = "/home.html" ? "/home.html" : "../home.html"} class="link">Inicio</a>
     <a href="/pages/pagina1.html" class="link">Pagina 1</a>
     <a href="/pages/pagina2.html" class="link">Pagina 2</a>
     <a href="/pages/pagina3.html" class="link">Pagina 3</a>

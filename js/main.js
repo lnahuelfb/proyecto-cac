6ceef46bc471771dd4270edde2a097bb797f1d6c
LNahuelFB <lnahuelfernandezb@gmail.com>
2024-03-30 05:52:11 -0300

    Avance en el header y creados los archivos de variables de CSS

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-const navbar = document.getElementById("navbar")
+const navbar = document.getElementById("header")
 const footer = document.getElementById("footer")
 
-let actualUrl = window.location.pathname
 const year = new Date().getFullYear()
+let actualUrl = window.location.pathname
 
-console.log('Hola mundo!');
 
 navbar.innerHTML = `
+  <picture>
+    <img src="/public/logo.svg" alt="logo" class="logo">
+  </picture>
+  <p>Proyecto grupal de Codo a Codo</p>
+  <nav class="navbar">
     <a href=${actualUrl = "/home.html" ? "/home.html" : "../home.html"} class="link">Inicio</a>
-    <a href="/pages/pagina1.html" class="link">Pagina 1</a>
-    <a href="/pages/pagina2.html" class="link">Pagina 2</a>
-    <a href="/pages/pagina3.html" class="link">Pagina 3</a>
+    <a href="/pages/detalle.html" class="link">Buscador</a>
+    <a href="/pages/azar.html" class="link">Al azar</a>
+    <a href="/pages/contacto.html" class="link">Contacto</a>
+  </nav>
 `;
 
 footer.innerHTML = `
-<p>Proyecto Full Stack con Python de Codo a Codo - ${year} </p>
-`
+  <img src="/public/cac.webp" alt="codo a codo" class="cac">
+  <p>Proyecto Full Stack con Python de Codo a Codo - ${year} </p>
+`;

9584d971ff325e0c0a6166468eed9d8059200dfb
LNahuelFB <lnahuelfernandezb@gmail.com>
2024-03-30 22:44:30 -0300

    Arreglo de las rutas

--- js/main.js
+++ js/main.js
@@ -6,21 +6,21 @@ let actualUrl = window.location.pathname.split('/')
 if (actualUrl[1] === "proyecto-cac") {
   navbar.innerHTML = `
   <picture>
-    <a href="/">
-      <img src=${actualUrl[2] === "pages" ? "../public/logo.svg" : "./public/logo.svg"} alt="logo" class="logo">
+    <a href="/proyecto-cac/">
+      <img src="/proyecto-cac/public/logo.svg" alt="logo" class="logo">
     </a>
   </picture>
   <p>Proyecto grupal de Codo a Codo</p>
   <nav class="navbar">
-    <a href="/" class="link">Inicio</a>
-    <a href=${actualUrl[2] === "pages" ? "detalle.html" : "proyecto-cac/pages/detalle.html"} class="link">Buscador</a>
-    <a href=${actualUrl[2] === "pages" ? "azar.html" : "proyecto-cac/pages/azar.html"} class="link">Al azar</a>
-    <a href=${actualUrl[2] === "pages" ? "contacto.html" : "proyecto-cac/pages/contacto.html"} class="link">Contacto</a>
+    <a href="/proyecto-cac/" class="link">Inicio</a>
+    <a href="/proyecto-cac/detalles" class="link">Buscador</a>
+    <a href="/proyecto-cac/azar" class="link">Al azar</a>
+    <a href="/proyecto-cac/contacto" class="link">Contacto</a>
   </nav>
   `;
 
   footer.innerHTML = `
-  <img src=${actualUrl[2] === "pages" ? "../public/cac.webp" : "./public/cac.webp"} alt="codo a codo" class="cac">
+  <img src="/proyecto-cac/public/cac.webp" alt="codo a codo" class="cac">
   <p>Proyecto Full Stack con Python de Codo a Codo - 2024 </p>
   `;
 }
@@ -28,19 +28,19 @@ if (actualUrl[1] === "proyecto-cac") {
 navbar.innerHTML = `
   <picture>
     <a href="/">
-      <img src=${actualUrl[1] === "pages" ? "../public/logo.svg" : "./public/logo.svg"} alt="logo" class="logo">
+      <img src="/public/logo.svg" alt="logo" class="logo">
     </a>
   </picture>
   <p>Proyecto grupal de Codo a Codo</p>
   <nav class="navbar">
     <a href="/" class="link">Inicio</a>
-    <a href=${actualUrl[1] === "pages" ? "detalle.html" : "pages/detalle.html"} class="link">Buscador</a>
-    <a href=${actualUrl[1] === "pages" ? "azar.html" : "pages/azar.html"} class="link">Al azar</a>
-    <a href=${actualUrl[1] === "pages" ? "contacto.html" : "pages/contacto.html"} class="link">Contacto</a>
+    <a href="/pages/detalle.html" class="link">Buscador</a>
+    <a href="/pages/azar.html" class="link">Al azar</a>
+    <a href="/pages/contacto.html" class="link">Contacto</a>
   </nav>
 `;
 
 footer.innerHTML = `
-  <img src=${actualUrl[1] === "pages" ? "../public/cac.webp" : "./public/cac.webp"} alt="codo a codo" class="cac">
+  <img src="/public/cac.webp" alt="codo a codo" class="cac">
   <p>Proyecto Full Stack con Python de Codo a Codo - 2024 </p>
 `;

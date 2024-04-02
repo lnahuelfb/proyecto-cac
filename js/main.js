24cf6208d0147e519dbe3e6d73316ab99cb328b5
LNahuelFB <lnahuelfernandezb@gmail.com>
2024-04-02 11:42:57 -0300

    prueba de cambio de rutas

--- js/main.js
+++ js/main.js
@@ -5,46 +5,46 @@ let [, actualUrl] = window.location.pathname.split('/')
 
 console.log("actual path: ", actualUrl)
 
-if (actualUrl === "proyecto-cac") {
-  navbar.innerHTML = `
-  <picture>
-    <a href="/proyecto-cac/">
-      <img src="/proyecto-cac/public/logo.svg" alt="logo" class="logo">
-    </a>
-  </picture>
-  <p>Proyecto grupal de Codo a Codo</p>
-  <nav class="navbar">
-    <a href="/proyecto-cac/" class="link">Inicio</a>
-    <a href="/proyecto-cac/pages/detalle" class="link">Buscador</a>
-    <a href="/proyecto-cac/pages/azar" class="link">Al azar</a>
-    <a href="/proyecto-cac/pages/contacto" class="link">Contacto</a>
-  </nav>
-  `
+// if (actualUrl === "proyecto-cac") {
+//   navbar.innerHTML = `
+//   <picture>
+//     <a href="/proyecto-cac/">
+//       <img src="/proyecto-cac/public/logo.svg" alt="logo" class="logo">
+//     </a>
+//   </picture>
+//   <p>Proyecto grupal de Codo a Codo</p>
+//   <nav class="navbar">
+//     <a href="/proyecto-cac/" class="link">Inicio</a>
+//     <a href="/proyecto-cac/detalle" class="link">Buscador</a>
+//     <a href="/proyecto-cac/azar" class="link">Al azar</a>
+//     <a href="/proyecto-cac/contacto" class="link">Contacto</a>
+//   </nav>
+//   `
 
-  footer.innerHTML = `
-  <img src="/proyecto-cac/public/cac.webp" alt="codo a codo" class="cac">
-  <p>Proyecto Full Stack con Python de Codo a Codo - 2024 </p>
-  `
-}
+//   footer.innerHTML = `
+//   <img src="/proyecto-cac/public/cac.webp" alt="codo a codo" class="cac">
+//   <p>Proyecto Full Stack con Python de Codo a Codo - 2024 </p>
+//   `
+// }
 
-if (actualUrl !== "proyecto-cac") {
+// if (actualUrl !== "proyecto-cac") {
   navbar.innerHTML = `
     <picture>
-      <a href="/">
-        <img src="/public/logo.svg" alt="logo" class="logo">
+      <a href="index.html">
+        <img src="public/logo.svg" alt="logo" class="logo">
       </a>
     </picture>
     <p>Proyecto grupal de Codo a Codo</p>
     <nav class="navbar">
-      <a href="/" class="link">Inicio</a>
-      <a href="/pages/detalle.html" class="link">Buscador</a>
-      <a href="/pages/azar.html" class="link">Al azar</a>
-      <a href="/pages/contacto.html" class="link">Contacto</a>
+      <a href="index.html" class="link">Inicio</a>
+      <a href="detalle.html" class="link">Buscador</a>
+      <a href="azar.html" class="link">Al azar</a>
+      <a href="contacto.html" class="link">Contacto</a>
     </nav>
   `
 
   footer.innerHTML = `
-    <img src="/public/cac.webp" alt="codo a codo" class="cac">
+    <img src="public/cac.webp" alt="codo a codo" class="cac">
     <p>Proyecto Full Stack con Python de Codo a Codo - 2024 </p>
   `
-}
+// }
